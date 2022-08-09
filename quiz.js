39104a09153fb527eabf4431a6358c86ad8da39f
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-09 13:46:42 +0530

    feat: added main logic of the app

--- quiz.js
+++ quiz.js
@@ -1,1 +1,26 @@
 let form = document.querySelector(".quiz-form");
+let submitButton = document.querySelector(".submit");
+let outputEl = document.querySelector(".quiz-output");
+const correctAnswers = ["90°", "right angled", "one right angle"];
+// const selectedAnswers = [];
+function calculateScore(event) {
+  event.preventDefault();
+  let formResults = new FormData(form);
+  let score = 0;
+  let index = 0;
+  for (let value of formResults.values()) {
+    // selectedAnswers.push(key[1]);
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  // for (let i = 0; i < correctAnswers.length; i++) {
+  //   if (correctAnswers[i] == selectedAnswers[i]) {
+  //     score = score + 1;
+  //   }
+  // }
+  outputEl.innerText = "Your total score is " + score;
+}
+
+submitButton.addEventListener("click", calculateScore);
