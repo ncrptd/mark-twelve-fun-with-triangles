693dad97237523cbc80a3fd7a6a5c106984ecff1
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-27 18:19:10 +0530

    added empty input validation

--- area.js
+++ area.js
@@ -7,7 +7,11 @@ function calculateArea() {
   let b = +sides[1].value;
   let c = +sides[2].value;
   let s = (a + b + c) / 2;
-  let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-  outputEl.innerText = `Area of a triangle is ${area} units`;
+  if (!a || !b || !c) {
+    outputEl.innerText = "Please enter valid input";
+  } else {
+    let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    outputEl.innerText = `Area of a triangle is ${area} units`;
+  }
 }
 areaButton.addEventListener("click", calculateArea);
